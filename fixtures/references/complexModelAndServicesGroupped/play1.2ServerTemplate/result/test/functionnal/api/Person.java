85cf63bd1c52a34417346c0d356ea2c9e4fce6c4
Médéric Andrieux <m.andrieux@omnium-systems.com>
2013-12-05 11:43:44 +0100

    add play server fixture template

---
+++ fixtures/references/complexModelAndServicesGroupped/play1.2ServerTemplate/result/test/functionnal/api/Person.java
@@ -0,0 +1,33 @@
+package functionnal.api;
+
+import controllers.api.results.WSParameterNames;
+import controllers.api.results.WSResult;
+import controllers.api.results.WSResultCode;
+import com.google.gson.Gson;
+import com.google.gson.internal.StringMap;
+import org.junit.*;
+import play.mvc.Http.Request;
+import play.mvc.Http.Response;
+import play.test.*;
+
+public class PersonTest extends FunctionalTest {
+
+    
+     
+     
+    @Test
+    public void testAddPerson() {
+        
+        String params = "firstname=default&amp;lastname=default&amp;birthdate=456&app_key=API_APP_KEY";
+        Response response = POST("/person?" + params);
+        assertIsOk(response);
+        
+        String responseBody = response.out.toString();
+        Gson gson = new Gson();
+        WSResult wsResult = gson.fromJson(responseBody, WSResult.class);
+        assertNotNull(wsResult);
+        assertEquals(WSResultCode.CODE_SUCCESS, wsResult.getCode());
+        assertTrue(wsResult.isSuccess()); 
+    }    
+    
+}
---
+++ fixtures/references/complexModelAndServicesGroupped/play1.2ServerTemplate/result/test/functionnal/api/Shop.java
@@ -0,0 +1,49 @@
+package functionnal.api;
+
+import controllers.api.results.WSParameterNames;
+import controllers.api.results.WSResult;
+import controllers.api.results.WSResultCode;
+import com.google.gson.Gson;
+import com.google.gson.internal.StringMap;
+import org.junit.*;
+import play.mvc.Http.Request;
+import play.mvc.Http.Response;
+import play.test.*;
+
+public class ShopTest extends FunctionalTest {
+
+    
+     
+     
+    @Test
+    public void testGetListShop() {
+        
+        String params = "latitude=578&amp;longitude=578&app_key=API_APP_KEY";
+        Response response = GET("/shop?" + params);
+        assertIsOk(response);
+        
+        String responseBody = response.out.toString();
+        Gson gson = new Gson();
+        WSResult wsResult = gson.fromJson(responseBody, WSResult.class);
+        assertNotNull(wsResult);
+        assertEquals(WSResultCode.CODE_SUCCESS, wsResult.getCode());
+        assertTrue(wsResult.isSuccess()); 
+    }    
+     
+     
+    @Test
+    public void testSetShopOwner() {
+        
+        String params = "personId=default&amp;shopId=default&app_key=API_APP_KEY";
+        Response response = PUT("/shop?" + params);
+        assertIsOk(response);
+        
+        String responseBody = response.out.toString();
+        Gson gson = new Gson();
+        WSResult wsResult = gson.fromJson(responseBody, WSResult.class);
+        assertNotNull(wsResult);
+        assertEquals(WSResultCode.CODE_SUCCESS, wsResult.getCode());
+        assertTrue(wsResult.isSuccess()); 
+    }    
+    
+}
